fdd6f18626fabc583e1c9506280dc07d3c6f6e01
Imagineee <imagineeelego@gmail.com>
2022-02-08 20:05:27 +0000

    added a base webpage

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 //deploy to github pages
 module.exports = {
-	base: '/vectocade/'
+	base: '/vectocade/',
+	title: 'Vectocade',
+	description: 'A Fantasy Computer for making tiny retro games, made with js',
+	//set the views directory
+	root: './src',
+	dest: './dist'
 }
