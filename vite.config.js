9878dc791b7e4e433f036c5df13c965f0f0da8e8
Imagineee <imagineeelego@gmail.com>
2022-02-09 08:00:22 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ module.exports = {
 	base: '/vectocade/',
 	title: 'Vectocade',
 	description: 'A Fantasy Computer for making tiny retro games, made with js',
-	//set the views directory
 	root: './src',
-	dest: './dist'
 }
