cae35d157123eed2ede913bdf95a086f907cb91a
Imagineee <imagineeelego@gmail.com>
2022-02-09 20:09:01 +0000

    added some text and some config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-//deploy to github pages
+const { resolve } = require('path')
 module.exports = {
 	base: '/vectocade/',
 	title: 'Vectocade',
 	description: 'A Fantasy Computer for making tiny retro games, made with js',
 	//set the views directory
 	root: './src',
-	dest: './dist'
+	dest: './dist',
+	build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        editor: resolve(__dirname, 'editor/index.html')
+      }
+    }
+  }
 }
